30a70adc36e576e7d094ea89555faf319fa09e3d
Noah Witt <noah@noah-witt.com>
2020-08-31 15:00:05 -0500

    working done

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import Got from 'got';
+import * as moment from 'moment-timezone';
+import { JSDOM } from 'jsdom';
+import { off } from 'process';
+
+interface dilbertComic {
+    url: string;
+    title: string;
+    date: string;
+}
+
+/**
+ * date of the first dilbert comic.
+ */
+export const dilbertStart = moment().second(0).minute(0).hour(12).date(16).month(3).year(1989);
+
+/**
+ * finds the newest dilbert comic and returns it.
+ * @throws dilbert get exception
+ */
+export async function getLatest():Promise<dilbertComic>{
+    let time = moment().add(1, 'day').startOf('day');
+    return await getByMomentObject(time);
+}
+
+/**
+ * returns the dilbert comic.
+ * @param date the date as a moment object.
+ * @throws dilbert get exception
+ */
+async function getByMomentObject(date: moment.Moment):Promise<dilbertComic> {
+    const formatted = date.format("YYYY-MM-DD");
+    return await getByDateString(formatted);
+}
+
+/**
+ * returns the time.
+ * @param date the date as a string
+ * @throws dilbert get exception
+ */
+export async function getByDateString(date: string):Promise<dilbertComic> {
+    try {
+        let response = await Got(`https://dilbert.com/strip/${date}`);
+        if(response.statusCode!==200) throw "request error";
+        const dom = new JSDOM(response.body);
+        const splitUrl =response.url.split('/');
+        return {
+            url: dom.window.document.querySelector('.img-responsive').getAttribute('src').trim(),
+            title: dom.window.document.querySelector('.comic-title-name').textContent.trim(),
+            date: splitUrl[splitUrl.length-1].trim()
+        }
+    } catch (error) {
+        console.log(error);
+        throw 'dilbert get exception';
+    }
+}
+/**
+ * @returns a random dilbert comic.
+ * @throws dilbert get exception
+ */
+export async function getRandom(): Promise<dilbertComic> {
+    const days = Math.floor(moment.duration(moment().startOf('day').diff(dilbertStart)).asDays());
+    const offset = Math.floor(Math.random() * (days + 1));
+    //console.log(offset);
+    const day = dilbertStart.clone().add(offset, 'day');
+    return await getByMomentObject(day);
+}
---
+++ test/test.js
@@ -0,0 +1,32 @@
+const dilbert = require('../build/index');
+var assert = require('assert');
+describe('Pull Some Comics', function() {
+    it('first dilbert right date', function() {
+        assert.equal(dilbert.dilbertStart.year(), 1989);
+        assert.equal(dilbert.dilbertStart.month(), 3);
+        assert.equal(dilbert.dilbertStart.date(), 16);
+    });
+    it('should not fail', async function() {
+        this.timeout(50000);
+        assert.ok((await dilbert.getLatest()).url);
+    })
+    it('should return the correct object', async function() {
+        this.timeout(50000);
+        const goal = {url: '//assets.amuniversal.com/5abe86d0bfb101381a80005056a9545d', title: 'No More Id Badges',date: '2020-08-31' };
+        const actual = await dilbert.getByDateString('2020-08-31');
+        assert.equal(goal.url, actual.url);
+        assert.equal(goal.title, actual.title);
+        assert.equal(goal.date, actual.date);
+    })
+    it('use random (this could fail randomly, very low odds', async function(){
+        this.timeout(50000);
+        const a = dilbert.getRandom();
+        const b = dilbert.getRandom();
+        const c = dilbert.getRandom();
+        assert.ok(await a);
+        assert.notEqual((await b).date, (await c).date);
+        const d = dilbert.getRandom();
+        const e = dilbert.getRandom();
+        assert.notEqual((await d).url, (await e).url);
+    });
+});
